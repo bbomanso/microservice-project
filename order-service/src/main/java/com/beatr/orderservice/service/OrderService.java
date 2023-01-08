64837db6c044e878fbcea00ad3569ef07c63a2bb
beat2797 <beatrizristau@gmail.com>
2023-01-08 10:31:48 -0500

    invetory updated

--- order-service/src/main/java/com/beatr/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/beatr/orderservice/service/OrderService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.beatr.orderservice.dto.OrderLineItemsDto;
 import com.beatr.orderservice.dto.OrderRequest;
@@ -14,6 +15,7 @@ import com.beatr.orderservice.entity.OrderLineItems;
 import com.beatr.orderservice.repository.OrderRepository;
 
 @Service
+@Transactional // will automatically create and commit the transactions
 public class OrderService {
 
     @Autowired
